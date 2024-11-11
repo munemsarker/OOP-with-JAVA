52a6a9ffd0516f67833a7928c1a00bec9658ca26
Munem Sarker <97896330+munemsarker@users.noreply.github.com>
2024-11-12 00:43:54 +0600

    first commit

---
+++ 09-11-2024/A_10ten.java
@@ -0,0 +1,9 @@
+public class A_10ten {
+    public static void main(String[]args){
+        System.out.println("Here are the first 10 natural numbers: ");
+        int i;
+        for (i=1;i<=10;i++){      
+		System.out.println(i+"\n");
+	}
+    }
+}
